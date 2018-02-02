e669692f36337ae5b4604215924ee90b1ed4a333
Jonathan Tung <jtung23@gmail.com>
2018-02-02 15:42:21 -0800

    first commit

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,15 @@
+import './Main.css';
+import React from "react";
+
+const Main = props => (
+	<div className="main">
+		<h1>
+			Jonathan Tung
+		</h1>
+		<p>
+
+		</p>
+	</div>
+)
+
+export default Main
